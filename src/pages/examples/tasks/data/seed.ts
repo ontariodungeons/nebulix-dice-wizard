78a4c8b9187e30708720e4babf1a065b302c2f14
Jesse Naiman <ontariodungeons@gmail.com>
2024-03-25 14:44:12 -0400

    Always make sure 'npx astro build' suceeds or it will not build on the server

--- src/pages/examples/tasks/data/seed.ts
+++ src/pages/examples/tasks/data/seed.ts
@@ -1,6 +1,10 @@
 import fs from "fs"
 import path from "path"
 import { faker } from "@faker-js/faker"
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import { labels, priorities, statuses } from "./data"
 
